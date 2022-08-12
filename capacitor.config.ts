ac2f82e766d921a0dd90baa0d8f2482ea5029c09
Ahmetcan Bozkurt <ahmedbozkurt959@gmail.com>
2022-08-12 19:20:51 +0300

    bitti.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'cryptonewapp',
+  appId: 'com.ahmedobozkurt.cryptoapp',
+  appName: 'cryptonewapp2',
   webDir: 'www',
   bundledWebRuntime: false
 };
